f13249b29c83558a6ae02a2c9f7fd841ceab5125
akramuadz <156046154+akramuadz@users.noreply.github.com>
2025-02-01 21:39:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Contoh: Alert saat halaman dimuat
+window.onload = function() {
+    alert("Selamat datang di website saya!");
+};
+
+// Contoh: Menambahkan efek klik ke tombol (jika ada)
+document.querySelector('nav a').addEventListener('click', function() {
+    console.log('Tombol diklik!');
+});
